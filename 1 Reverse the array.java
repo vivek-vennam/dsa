15fe4ece0ea34551ac82dad0dc45494debb2d3e9
vivek-vennam <109010905+vivek-vennam@users.noreply.github.com>
2022-11-15 07:04:16 +0530

    solution added

---
+++ 1 Reverse the array.java
@@ -0,0 +1,15 @@
+import java.io.*;
+ 
+class ReverseArray {
+ 
+    static void rvereseArray(int arr[], int start, int end)
+    {
+        int temp;
+        if (start >= end)
+            return;
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        rvereseArray(arr, start+1, end-1);
+    }
+}
